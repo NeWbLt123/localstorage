c965752e856389b6817e6320635538c575ca6ca4
Antoine Cloutier <cloutier.antoine@gmail.com>
2014-06-23 09:58:33 -0400

    Create localstoragejs.js

---
+++ localstoragejs.js
@@ -0,0 +1,16 @@
+(function(global){
+    if(!$.cookie) throw "Please include jQuery cookie before this script.";
+    
+    var _storage = global.storage = global.storage || {}
+	var _method = Modernizr.localstorage ? "localstorage" : "cookie";
+    
+    _storage.get = function(key){
+        if(_method === "localstorage") return localStorage.getItem(key);
+    	if(_method === "cookie") return $.cookie(key);
+    }
+    
+    _storage.set = function(key, value){
+        if(_method === "localstorage") return localStorage.setItem(key, value);
+        if(_method === "cookie") return $.cookie(key, value);
+    }
+})(window);
